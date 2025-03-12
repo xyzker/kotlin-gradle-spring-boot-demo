280e041eaa26b5cdb5a3762d45647f6af3c6ef0f
xyzker <410317669@qq.com>
2025-03-12 17:35:57 +0800

    update

--- src/main/kotlin/com/example/repository/GithubRepository.kt
+++ src/main/kotlin/com/example/repository/GithubRepository.kt
@@ -1,4 +1,4 @@
-package com.example.web
+package com.example.repository
 
 import org.kohsuke.github.GHRepository
 import org.kohsuke.github.GitHub
@@ -9,4 +9,10 @@ class GithubRepository (
     val github: GitHub
 ){
     fun getRepo(name: String) : GHRepository = github.getRepository(name)
+
+    fun getLatestCommitId(name: String) : String {
+        val gHRepository = getRepo(name)
+        // 获取 main 分支的最新 commit ID
+        return gHRepository.getBranch("main").shA1
+    }
 }
---
+++ src/test/kotlin/com/example/repository/GithubRepositoryTest.kt
@@ -0,0 +1,44 @@
+package com.example.repository
+
+import com.example.config.BeanConfig
+import org.junit.jupiter.api.Test
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+
+@SpringBootTest(
+    classes = [
+        GithubRepository::class,
+        BeanConfig::class
+    ]
+)
+@EnableAutoConfiguration(exclude = [
+    DataSourceAutoConfiguration::class,
+    DataSourceTransactionManagerAutoConfiguration::class,
+    HibernateJpaAutoConfiguration::class
+])
+@ActiveProfiles("dev")
+class GithubRepositoryTest {
+
+    @Autowired
+    private lateinit var githubRepository: GithubRepository
+
+    val repoName = "xyzker/kotlin-gradle-spring-boot-demo"
+
+    @Test
+    fun getRepo() {
+        val repo = githubRepository.getRepo(repoName)
+        println(repo)
+    }
+
+    @Test
+    fun getLatestCommitId() {
+        val commitId = githubRepository.getLatestCommitId(repoName)
+        println(commitId)
+    }
+}
